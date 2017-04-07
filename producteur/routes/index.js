4d2e403f0c6eeeb0f2718074bd7ea187c7ddd5d8
nsutter <nsutter@etu.unistra.fr>
2017-04-07 16:56:55 +0200

    [Ajout] implémentations des fonctions producteur hormis start

--- producteur/routes/index.js
+++ producteur/routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
+var ressource;
+var nombre;
+var delay= 300;
+
+function update()
+{
+  nombre ++;
+  console.log(nombre);
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// lancement de la partie
+router.post('/start', function(req, res, next){
+  setTimeout(update, delay);
+});
+
+//connaitre le type de ressource et le nombre que le producteur possède
+router.get('/show_ressource', function(req, res, next){
+  res.send(ressource + ": " + nombre);
+});
+
+//prendre une ressource
+router.get('/get_ressource/:nb', function(req, res, next){
+  if(req.param.nb < nombre){
+    nombre= nombre - req.param.nb;
+    res.send(req.param.nb);
+  }
+  else {
+    res.send(nombre);
+    nombre= 0;
+  }
 });
 
 module.exports = router;
