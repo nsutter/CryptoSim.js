1f7060f3f8a31bbe24cab2761309f6717d0edf14
Théo Poizat <theo.poizat@etu.unistra.fr>
2017-04-09 17:58:40 +0200

    Migration des fonctions de joueur dans un fichier spécifique

---
+++ joueur/private/joueur.js
@@ -0,0 +1,32 @@
+module.exports = {
+  /*
+    renvoie la quantité volée de la ressource volée
+    TODO : seuil maximum de quantité volée
+  */
+  vol: function(ressourceVolee, quantiteVolee){
+
+    // si on a pas le droit de voler, on s'arrête
+    if(!voler)
+      return 0;
+
+    for(var i = 0; i < ressources.length; i++)
+    {
+      if(ressources[i].nom = ressourceVolee)
+      {
+        if(ressources[i].quantite >= quantiteVolee)
+        {
+          ressources[i].quantite -= quantiteVolee;
+          return quantiteVolee; // vol total
+        }
+        else
+        {
+          var quantite = ressources[i].quantite;
+          ressources[i].quantite = 0;
+          return quantite; // vol partiel
+        }
+      }
+    }
+
+    return 0;
+  }
+};
