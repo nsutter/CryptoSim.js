26b456af47999cc1c8fafe915a2e73eacd64eb55
Théo Poizat <theo.poizat@etu.unistra.fr>
2017-04-15 16:05:16 +0200

    1ère règles générales et objectifs de jeu

--- coordinateur/routes/index.js
+++ coordinateur/routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/*
+etat = 0 -> rien
+etat = 1 -> regles OK
+etat = 2 -> regles OK, ..
+*/
+var etat = 0;
+
+// Paramétrage des règles et des agents
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Préparation du jeu 1/2 - Paramétrage des règles et des agents' });
 });
+
+
+// Enregistrement des règles et des agents
+router.post('/regles', function(req, res, next) {
+
+  console.log(req.body);
+
+  // enregistrer les règles
+  etat = 1;
+
+  // lancer les agents
+
+  res.redirect('/');
+});
+
+
+// Inscription des agents
+router.get('/inscription', function(req, res, next) {
+  res.render('inscription', { title: 'Préparation du jeu 2/2 - Inscription des agents' });
+});
+
+
+
 
 module.exports = router;
