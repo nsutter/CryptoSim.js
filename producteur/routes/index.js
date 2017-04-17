e1ca9ab5a9e6bce9421202f7a2b5c0a42bb8d57d
nsutter <nsutter@etu.unistra.fr>
2017-04-17 19:51:25 +0200

    [Producteur] ajout d'un commentaire

--- producteur/routes/index.js
+++ producteur/routes/index.js
@@ -8,10 +8,13 @@ module.exports = router;
 var delay= 1000;
 var serveur= "localhost:4000"
 
+var quantite= param.quantite;
+
+//fonction de production de la ressource: fonction qu'on peut faire nombre_initial/2*quantite
 function update()
 {
-  param.quantite = param.quantite + param.quantite_produit;
-  console.log(param.quantite);
+  quantite = quantite + quantite_produit;
+  console.log(quantite);
   setTimeout(update, delay);
 }
 
@@ -25,19 +28,19 @@ router.get('/start', function(req, res, next){
   setTimeout(update, delay);
 });
 
-//connaitre le type de ressource et le param.quantite que le producteur possède
+//connaitre le type de ressource et le quantite que le producteur possède
 router.get('/show_ressource', function(req, res, next){
-  res.send(param.ressource + ": " + param.quantite);
+  res.send(param.ressource + ": " + quantite);
 });
 
 //prendre une ressource
 router.get('/get_ressource/:nb', function(req, res, next){
-  if(parseInt(req.params.nb) <= param.quantite){
-    param.quantite= param.quantite - parseInt(req.params.nb);
+  if(parseInt(req.params.nb) <= quantite){
+    quantite= quantite - parseInt(req.params.nb);
     res.send(req.params.nb);
   }
   else {
-    res.send(param.quantite);
-    param.quantite= 0;
+    res.send(quantite);
+    quantite= 0;
   }
 });
